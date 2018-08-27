3d60dcad1875194eb2c2cdebf9a0747ef94d7009
KevinP Fowler <kevpfowler@gmail.com>
2018-08-27 11:22:32 -0400

    Modified lib/inc config to have fallback if not envvars, so RLS is happy

--- zmq-sys/build.rs
+++ zmq-sys/build.rs
@@ -1,19 +1,35 @@
 use std::env;
 use std::path::Path;
 
-fn prefix_dir(env_name: &str, dir: &str) -> String {
+// Fallbacks if env vars not set - so RLS can compile the crate
+const LIBZMQ_PREFIX_FALLBACK: &'static str = "/usr/lib/arm-linux-gnueabihf";
+const INCZMQ_PREFIX_FALLBACK: &'static str = "/usr/include";
+
+fn lib_prefix_dir(env_name: &str, dir: &str) -> String {
     match env::var(env_name) {
         Ok(value) => value,
         Err(..) => match env::var("LIBZMQ_PREFIX") {
             Ok(prefix) => Path::new(&prefix).join(dir).to_string_lossy().into_owned(),
-            Err(..) => panic!("Please set the {} or LIBZMQ_PREFIX environment variable", env_name)
+            Err(..) => { 
+                println!("Neither {} nor LIBZMQ_PREFIX environment variables set, using fallback", env_name);
+                LIBZMQ_PREFIX_FALLBACK.to_string()
+            }
+        }
+    }
+}
+fn inc_prefix_dir(env_name: &str) -> String {
+    match env::var(env_name) {
+        Ok(value) => value,
+        Err(..) => {
+            println!("LIBZMQ_INCLUDE_DIR environment variable not set, using fallback");
+            INCZMQ_PREFIX_FALLBACK.to_string()
         }
     }
 }
 
 fn main() {
-    let lib_path = prefix_dir("LIBZMQ_LIB_DIR", "lib");
-    let include = prefix_dir("LIBZMQ_INCLUDE_DIR", "include");
+    let lib_path = lib_prefix_dir("LIBZMQ_LIB_DIR", "lib");
+    let include = inc_prefix_dir("LIBZMQ_INCLUDE_DIR");
 
     println!("cargo:rustc-link-search=native={}", lib_path);
     println!("cargo:include={}", include);
