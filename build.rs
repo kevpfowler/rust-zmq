3ee21e6c770bae9d65a931556a99b2eeb8a39519
KevinP Fowler <kevpfowler@gmail.com>
2018-08-27 11:36:28 -0400

    Modified ZMQ_HAS config to have fallback if not envvar, so RLS is happy

--- build.rs
+++ build.rs
@@ -1,9 +1,15 @@
 use std::env;
 
+// Fallback if env var not set - so RLS can compile the crate
+const ZMQ_HAS_FALLBACK: &'static str = "";
+
 fn main() {
     let zmq_has = match env::var("ZMQ_HAS") {
         Ok(value) => value,
-        Err(..) => panic!("Please set the ZMQ_HAS environment variable (e.g. \"ipc,pgm,tipc,norm,curve,gssapi\")")
+        Err(..) =>  { 
+            println!("ZMQ_HAS environment variable not set, using fallback");
+            ZMQ_HAS_FALLBACK.to_string()
+        }
     };
 
     println!("rerun-if-env-changed=ZMQ_HAS");
